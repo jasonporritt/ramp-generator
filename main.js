86b7389f9b4f14063934076957f74c52be43bdf0
Jason Porritt <jason.porritt@atomicobject.com>
2016-05-02 23:10:16 -0400

    Changed the ramping calculation to use color mixing.

--- main.js
+++ main.js
@@ -8,18 +8,34 @@ var makeBlock = function(color, totalCount) {
 
 $(function() {
   $("#generate-ramp-form").submit(function() {
-    var numberOfColors = $("#number-of-colors").val();
+    var numberOfSteps = $("#number-of-colors").val();
+    var totalColorsCount = numberOfSteps*2 + 1;
     var saturationShiftPerStep = $("#saturation-shift-per-step").val() / 100;
-    var valueShiftPerStep = $("#value-shift-per-step").val() / 100;
+    var lightValue = $("#light-value").val();
+    var lightSaturation = $("#light-saturation").val();
+    var darkValue = $("#dark-value").val();
+    var darkSaturation = $("#dark-saturation").val();
     var hueShiftPerStep = $("#hue-shift-per-step").val();
-    var middleColor = $("#middle-color-value").val();
+    var middleColor = Color($("#middle-color-value").val());
     var updatedContent = [];
 
-    for (var i = -1*numberOfColors; i <= numberOfColors; i++) {
-      var color = Color(middleColor).
-        rotate(hueShiftPerStep * i).
-        darken(valueShiftPerStep * i).saturate(saturationShiftPerStep * i);
-      var div = makeBlock(color, numberOfColors*2 + 1);
+    var lightColor = middleColor.clone().rotate(hueShiftPerStep * numberOfSteps * -1).lightness(lightValue).saturation(lightSaturation);
+    var darkColor  = middleColor.clone().rotate(hueShiftPerStep * numberOfSteps).lightness(darkValue).saturation(darkSaturation);
+    // updatedContent.push(makeBlock(lightColor, totalColorsCount));
+    // updatedContent.push(makeBlock(middleColor, totalColorsCount));
+    // updatedContent.push(makeBlock(darkColor, totalColorsCount));
+
+    console.log(lightColor.hexString());
+    console.log(darkColor.hexString());
+    for (var i = 0; i < numberOfSteps; i++) {
+      var color = middleColor.clone().mix(lightColor.clone(), i * (1.0/numberOfSteps));
+      var div = makeBlock(color, totalColorsCount);
+      updatedContent.push(div);
+    }
+    updatedContent.push(makeBlock(middleColor, totalColorsCount));
+    for (var i = 0; i < numberOfSteps; i++) {
+      var color = middleColor.clone().mix(darkColor.clone(), (numberOfSteps - i - 1) * (1.0/numberOfSteps));
+      var div = makeBlock(color, totalColorsCount);
       updatedContent.push(div);
     }
     $("#ramp-results").prepend('<div class="ramp-group">' + updatedContent.join("\n") + '<a class="remove" href="#">Remove</a></div>');
